2802cd36845715bab88859f8e4ce303609c0ee28
vronsser <valentin.ronsseray@etu.utc.fr>
2024-04-27 17:03:12 +0200

    Documentation generateRandomDistinctIntegers

--- Code/utils.cpp
+++ Code/utils.cpp
@@ -8,8 +8,18 @@
 #include <set>
 #include <ctime>
 
-// Fonction pour générer un tableau d'entiers distincts aléatoires
+/**
+ * Génère un tableau d'entiers aléatoires distincts dans une plage donnée.
+ *
+ * @param[in] min La valeur minimale de la plage (incluse).
+ * @param[in] max La valeur maximale de la plage (incluse).
+ * @param[in] count Le nombre d'entiers à générer.
+ * @return Un tableau d'entiers aléatoires distincts.
+ * @exemple generateRandomDistinctIntegers(1, 10, 5) peut renvoyer {3, 1, 5, 9, 7}.
+ */
 std::vector<int> generateRandomDistinctIntegers(int size, int min, int max) {
+
+
     // Initialisation du générateur de nombres aléatoires
     std::random_device rd;
     std::mt19937 gen(rd());
