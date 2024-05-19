93656cd106cbac1b03e65c089a4969ba55f71357
Chloe Taurel <chloe.taurel@etu.utc.fr>
2024-05-19 15:18:58 +0000

    Update file batiment.cpp

--- Code/batiment.cpp
+++ Code/batiment.cpp
@@ -11,66 +11,6 @@ bool Batiment::est_chainée(const Joueur &j) {
     }
     return false;
 };
-Commerce::Commerce(const string &n, const unsigned int &cout_piece,
-                   const unsigned int &cout_bois,const unsigned int &cout_argile,
-                   const unsigned int &cout_pierre, const unsigned int &cout_verre,
-                   const unsigned int &cout_papyrus, const int &a,const bool &f,
-                   const bool &ac, const statut &s, const string &c,
-                   const int &p, const int &solde,const Ressource &aff,
-                   const unsigned int &nb_bois, const unsigned int &nb_argile,
-                   const unsigned int &nb_pierre, const unsigned int &nb_verre,
-                   const unsigned int &nb_papyrus,const string& t,const int& age)
-                   : Batiment::Batiment(n, cout_piece,
-                                        cout_bois, cout_argile,
-                                        cout_pierre, cout_verre,
-                                        cout_papyrus, a, f, ac, s, c,t,age),
-                                        points(p), solde_apporte(solde),
-                                        affecte(aff){
-                                        int taille_rand=0;
-                                        int ressource_rand[5];
-                                        if(nb_bois>0){
-                                            taille_rand++;
-                                            ressource_rand[0]=nb_bois;
-                                        }
-                                        if(nb_argile>0){
-                                            taille_rand++;
-                                            ressource_rand[1]=nb_argile;
-                                        }
-                                        if(nb_pierre>0){
-                                            taille_rand++;
-                                            ressource_rand[2]=nb_pierre;
-                                        }
-                                        if(nb_verre>0){
-                                            taille_rand++;
-                                            ressource_rand[3]=nb_verre;
-                                        }
-                                        if(nb_papyrus>0){
-                                            taille_rand++;
-                                            ressource_rand[4]=nb_papyrus;
-                                        }
-                                        int x=rand()%taille_rand + 1;
-                                        if(taille_rand){
-                                            switch (x){
-                                                case 1:
-                                                    this->nb_bois=ressource_rand[0];
-                                                    break;
-                                                case 2:
-                                                    this->nb_argile=ressource_rand[1];
-                                                    break;
-                                                case 3:
-                                                    this->nb_pierre=ressource_rand[2];
-                                                    break;
-                                                case 4:
-                                                    this->nb_verre=ressource_rand[3];
-                                                    break;
-                                                case 5:
-                                                    this->nb_papyrus=ressource_rand[4];
-                                                    break;
-                                            }
-                                        }
-
-}
-
 
 
 Merveille::Merveille(const string& n, const unsigned int &cout_piece,
