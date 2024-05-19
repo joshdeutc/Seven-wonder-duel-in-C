3a32ade41f7e3912e7cbe39316a77776965b3e51
Chloe Taurel <chloe.taurel@etu.utc.fr>
2024-05-19 14:06:29 +0000

    Update file partie.h

--- Code/partie.h
+++ Code/partie.h
@@ -28,6 +28,16 @@ public:
     void tour_suivant();
     void fin_age();
     void choix_merveilles();
+    void selection_action(Joueur* j); //Fonction que doit faire Joshua. C'est celle qui permet au joueur j de choisir
+    //ce qu'il veut faire (construire un batiment ou une merveille)
+    // IL EXISTE UNE FONCTION DOUBLE_SYMBOLE(STRING S) DANS LA CLASSE JOUEUR QUI PERMET DE VÉRIFIER SI LE SYMBOLE ENTRÉ
+    // EN ARGUMENT EST DÉJÀ DANS LA CITÉ DU JOUEUR!! IL FAUT UTILISER CETTE FONCTION QD ON CONSTRUIT UN BATIMENT
+    //SCIENTIFIQUE POUR SAVOIR SI LE JOUEUR PEUT CHOISIR UN JETON!!
+    void choix_jeton(Joueur* j); //Fonction appelée par selection_action() si le joueur a choisi une action qui permet
+    //de construire un jeton (par ex si il a construit un batiment scientifique et que ça lui a apporté un symbole
+    //identique à un de ceux qu'il a déjà dans sa cité). Cette fonction permet donc au joueur de sélectionner le jeton
+    //qu'il souhaite. Il faut que cette méthode appelle la méthode de joueur ajouter_jeton() une fois le jeton choisi
+    // car cette méthode de joueur s'occupe de mettre ses attributs à jour.
     void victoire_scientifique(Joueur j);
     void change_solde_militaire(signed int nb_boucliers);
     void victoire_militaire();
