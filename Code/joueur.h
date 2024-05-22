d3153a0260f7436bddf49b21a0b53cb5e2ce9682
Chloe Taurel <chloe.taurel@etu.utc.fr>
2024-05-22 18:38:59 +0000

    Update file joueur.h

--- Code/joueur.h
+++ Code/joueur.h
@@ -18,32 +18,31 @@ class Joueur{
     string id;
     int points = 0;
     int solde = 0;
-    int ressources_prod[5] = {0,0,0,0,0};
-    int ressources_non_prod[5] = {0,0,0,0,0}; //Pour les cartes Forum et Caravanserail
-    int symboles[7] = {0,0,0,0,0,0,0}; //On se servira de l'enum pour acceder aux symboles par leur nom
-    int nb_batMax = 10; //Taille du tableau de pointeurs vers batiments
-    Batiment* batiments[nb_batMax];
-    int nb_bat = 0;
-    Merveille* merveilles[7];
-    int nb_mer = 0;
+    int ressources_prod[NB_RESSOURCES] = {0,0,0,0,0}; //Inutile de stocker la valeur nulle
+    int ressources_non_prod[NB_RESSOURCES] = {0,0,0,0,0}; //Inutile de stocker la valeur nulle
+    int symboles[NB_SYMB] = {0,0,0,0,0,0,0}; //On se servira de l'enum pour acceder aux symboles par leur nom
+    int nb_cartesMax = 10; //Taille du tableau de pointeurs vers batiments
+    const Carte** cartes;
+    int nb_cartes = 0;
     JetonProgres* jetons[NB_JETONS];
     int nb_jetons=0;
 public:
     Joueur(TypeJoueur type_joueur, string identifiant);
-    int nb_symboles() const;
-    bool double_symbole(string s); // Vaut-il mieux mettre la carte en argument ou son symbole direct?
+    ~Joueur();
+    int nbSymboles() const;
+    bool doubleSymbole(SymboleScientifique s); // Vaut-il mieux mettre la carte en argument ou son symbole direct?
     void ajouter_jeton(JetonProgres* jeton);
     void action();
     bool prix_fixe(Ressource r);
-    void defausser(Batiment* bat);
-    void construire_batiment(const Batiment& bat);
+    void defausser();
+    void ajouter_carte(const Carte& c);
+    void construire_carte(const Carte& c);
+    bool possede_chainage(string Nom_Chaine);
     void construire_merveille();
-    int get_couleur(couleur1,couleur2);
+    int get_couleur(TypeCarte couleur1,TypeCarte couleur2);
     void choisir_merveille();
-    int get_solde() const {return solde;}
-    void set_solde(int &s) { solde=s;}
-    void add_carte(const Carte& c);
 };
+
 
 
 
